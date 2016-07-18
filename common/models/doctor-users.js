8c17e09b9f6e78269d491dba76172d56fe26081a
Oscar M <omiranda@softstackfactory.org>
2016-07-18 23:40:05 +0000

    new backend

---
+++ common/models/doctor-users.js
@@ -0,0 +1,13 @@
+module.exports = function(Doctorusers) {
+Doctorusers.observe('after save', function(ctx, next) {
+        if(ctx.isNewInstance === true) {
+            var instance = ctx.instance;
+            instance.createAccessToken(1209600000, function(err, response) {
+                if(err === null) ctx.instance["token"] = response.id;
+                next();
+            });
+        }else {
+             next();
+        }
+    });
+};
